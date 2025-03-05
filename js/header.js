fc3ad2723bb52447312530544466a863648b0ff4
Will Procter <will.procter@gmail.com>
2025-03-05 20:05:59 +0000

    update

---
+++ js/header.js
@@ -0,0 +1,9 @@
+// This script injects the common head content.
+document.head.innerHTML = `
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Oakwood Leisure Centre</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/styles.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    
