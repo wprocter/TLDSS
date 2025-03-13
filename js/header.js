5bd3c45f8d5e0114bd16392a72eb94b26304fe52
Will Procter <procterw@bpc.ac.uk>
2025-03-13 15:56:52 +0000

    so it begins

--- js/header.js
+++
@@ -1,7 +0,0 @@
-document.head.innerHTML = `
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Oakwood Leisure Centre</title>
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/styles.css" rel="stylesheet">
-`;
