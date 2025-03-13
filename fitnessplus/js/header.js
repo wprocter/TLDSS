5f93c59a2dabce446c776b98b255949619822523
Will Procter <procterw@bpc.ac.uk>
2025-03-13 09:59:40 +0000

    fitnessplus

---
+++ fitnessplus/js/header.js
@@ -0,0 +1,7 @@
+document.head.innerHTML = `
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Oakwood Leisure Centre</title>
+  <link href="../core/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/styles.css" rel="stylesheet">
+`;
