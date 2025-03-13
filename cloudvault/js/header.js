f5d96b87fb9b0f0a45197f675199f3d50a6c3e86
Will Procter <procterw@bpc.ac.uk>
2025-03-13 16:24:49 +0000

    cloudvaultstarts

---
+++ cloudvault/js/header.js
@@ -0,0 +1,9 @@
+document.head.innerHTML = `
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CloudVault</title>
+  <link href="../core/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/styles.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="../core/js/jquery-3.6.0.min.js"></script>
+`;
